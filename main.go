afd22e2bb9a5a289adf457b2cd99eb96acfc3cc5
Hugo-Lourenco <hugo.lourenco@ynov.com>
2026-01-12 10:41:16 +0100

    main avec local host

--- main.go
+++ main.go
@@ -1,6 +1,26 @@
 package main
 
 import (
-"net/http"
-"groupie-tracker/api"
+    "fmt"
+    "net/http"
+    "groupie-tracker/api"
 )
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+    artists, err := api.GetArtists()
+    
+    if err != nil {
+        http.Error(w, "Erreur du serveur", http.StatusInternalServerError)
+        return
+    }
+
+   
+    fmt.Fprintf(w, "J'ai trouvé %d artistes !", len(artists))
+}
+
+func main() {
+    fmt.Println("Le serveur démarre sur http://localhost:8080")
+    
+    http.HandleFunc("/", HomeHandler)
+    http.ListenAndServe(":8080", nil)
+}
