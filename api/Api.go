541bdc8a06a2b725b98386d53bd6ab6bf5a5cb84
ethan <darklife.0992@gmail.com>
2026-01-12 14:10:41 +0100

    Ajout de l'API Location

--- api/Api.go
+++ api/Api.go
@@ -7,6 +7,7 @@ import (
 )
 
 const UrlArtists = "https://groupietrackers.herokuapp.com/api/artists"
+const UrlLocations = "https://groupietrackers.herokuapp.com/api/locations"
 
 func GetArtists() ([]models.Artist, error ) {
 
@@ -23,6 +24,23 @@ func GetArtists() ([]models.Artist, error ) {
 		return nil, err 
 	}
 	return artists, nil
+}
+
+func GetLocations() ([]models.Location, error) {
+
+	resp, err := http.Get(UrlLocations)
+
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var locationResponse models.LocationResponse
+	err = json.NewDecoder(resp.Body).Decode(&locationResponse)
+	if err != nil {
+		return nil, err
+	}
+	return locationResponse.Index, nil
 }
 	
 
