9bb1adf7c72d9d090df926521de7ebe96e00c420
Hugo-Lourenco <hugo.lourenco@ynov.com>
2026-01-22 23:07:17 +0100

    ajout des likes, de la géolocalisation et de spotify

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"log"
+	"net/url"
 	"strconv"
 	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 
 	"groupie-tracker/api"
@@ -19,6 +21,8 @@ func main() {
 	myWindow := myApp.NewWindow("Groupie Tracker")
 	myWindow.Resize(fyne.NewSize(500, 700))
 
+	LoadLikes()
+
 	artists, err1 := api.GetArtists()
 	relationsData, err2 := api.GetRelations()
 	locations, err3 := api.GetLocations()
@@ -29,19 +33,21 @@ func main() {
 
 	artistesAffiches := artists
 
+	checkFavorites := widget.NewCheck(" Afficher seulement mes favoris", nil)
+
 	labelDateCreation := widget.NewLabel("Date de création:")
-	sliderDateMin := widget.NewSlider(1960, 2020)
-	sliderDateMin.SetValue(1960)
-	sliderDateMax := widget.NewSlider(1960, 2020)
-	sliderDateMax.SetValue(2020)
-	labelDateValues := widget.NewLabel("1960 - 2020")
+	sliderDateMin := widget.NewSlider(1950, 2025)
+	sliderDateMin.SetValue(1950)
+	sliderDateMax := widget.NewSlider(1950, 2025)
+	sliderDateMax.SetValue(2025)
+	labelDateValues := widget.NewLabel("1950 - 2025")
 
 	labelAlbum := widget.NewLabel("Premier album:")
-	sliderAlbumMin := widget.NewSlider(1960, 2020)
-	sliderAlbumMin.SetValue(1960)
-	sliderAlbumMax := widget.NewSlider(1960, 2020)
-	sliderAlbumMax.SetValue(2020)
-	labelAlbumValues := widget.NewLabel("1960 - 2020")
+	sliderAlbumMin := widget.NewSlider(1950, 2025)
+	sliderAlbumMin.SetValue(1950)
+	sliderAlbumMax := widget.NewSlider(1950, 2025)
+	sliderAlbumMax.SetValue(2025)
+	labelAlbumValues := widget.NewLabel("1950 - 2025")
 
 	labelMembres := widget.NewLabel("Nombre de membres:")
 	sliderMembresMin := widget.NewSlider(1, 10)
@@ -56,10 +62,9 @@ func main() {
 	selectLieu := widget.NewSelect(lieux, nil)
 	selectLieu.SetSelected("Tous")
 
-	boutonFiltrer := widget.NewButton("Appliquer les filtres", nil)
-
+	boutonFiltrer := widget.NewButtonWithIcon("Appliquer les filtres", theme.SearchIcon(), nil)
 	barreRecherche := widget.NewEntry()
-	barreRecherche.SetPlaceHolder("Rechercher...")
+	barreRecherche.SetPlaceHolder("Rechercher un artiste, membre...")
 
 	var list *widget.List
 
@@ -76,51 +81,66 @@ func main() {
 			lieuChoisi = selectLieu.Selected
 		}
 
-		artistesFiltres := FiltrerArtistes(artists, locations, dateMin, dateMax, albumMin, albumMax, membresMin, membresMax, lieuChoisi)
+		tempArtistes := FiltrerArtistes(artists, locations, dateMin, dateMax, albumMin, albumMax, membresMin, membresMax, lieuChoisi)
 
 		if barreRecherche.Text != "" {
-			artistesAffiches = RechercheArtiste(barreRecherche.Text, artistesFiltres, locations)
-		} else {
-			artistesAffiches = artistesFiltres
+			tempArtistes = RechercheArtiste(barreRecherche.Text, tempArtistes, locations)
 		}
 
-		list.Refresh()
+		if checkFavorites.Checked {
+			onlyLikes := []models.Artist{}
+			for _, art := range tempArtistes {
+				if IsLiked(art.ID) {
+					onlyLikes = append(onlyLikes, art)
+				}
+			}
+			artistesAffiches = onlyLikes
+		} else {
+			artistesAffiches = tempArtistes
+		}
+
+		if list != nil {
+			list.Refresh()
+		}
 	}
 
-	sliderDateMin.OnChanged = func(v float64) {
+	updateLabels := func() {
 		labelDateValues.SetText(strconv.Itoa(int(sliderDateMin.Value)) + " - " + strconv.Itoa(int(sliderDateMax.Value)))
-	}
-	sliderDateMax.OnChanged = func(v float64) {
-		labelDateValues.SetText(strconv.Itoa(int(sliderDateMin.Value)) + " - " + strconv.Itoa(int(sliderDateMax.Value)))
-	}
-	sliderAlbumMin.OnChanged = func(v float64) {
 		labelAlbumValues.SetText(strconv.Itoa(int(sliderAlbumMin.Value)) + " - " + strconv.Itoa(int(sliderAlbumMax.Value)))
-	}
-	sliderAlbumMax.OnChanged = func(v float64) {
-		labelAlbumValues.SetText(strconv.Itoa(int(sliderAlbumMin.Value)) + " - " + strconv.Itoa(int(sliderAlbumMax.Value)))
-	}
-	sliderMembresMin.OnChanged = func(v float64) {
 		labelMembresValues.SetText(strconv.Itoa(int(sliderMembresMin.Value)) + " - " + strconv.Itoa(int(sliderMembresMax.Value)))
 	}
-	sliderMembresMax.OnChanged = func(v float64) {
-		labelMembresValues.SetText(strconv.Itoa(int(sliderMembresMin.Value)) + " - " + strconv.Itoa(int(sliderMembresMax.Value)))
-	}
+	sliderDateMin.OnChanged = func(f float64) { updateLabels() }
+	sliderDateMax.OnChanged = func(f float64) { updateLabels() }
+	sliderAlbumMin.OnChanged = func(f float64) { updateLabels() }
+	sliderAlbumMax.OnChanged = func(f float64) { updateLabels() }
+	sliderMembresMin.OnChanged = func(f float64) { updateLabels() }
+	sliderMembresMax.OnChanged = func(f float64) { updateLabels() }
 
 	boutonFiltrer.OnTapped = appliquerFiltres
-
-	barreRecherche.OnChanged = func(texte string) {
-		appliquerFiltres()
-	}
+	barreRecherche.OnChanged = func(s string) { appliquerFiltres() }
+	checkFavorites.OnChanged = func(b bool) { appliquerFiltres() }
 
 	list = widget.NewList(
 		func() int { return len(artistesAffiches) },
 		func() fyne.CanvasObject {
-			label := widget.NewLabel("Nom de l'artiste")
+			icon := widget.NewIcon(theme.CheckButtonCheckedIcon())
+			label := widget.NewLabel("Nom Artiste")
 			label.TextStyle = fyne.TextStyle{Bold: true}
-			return label
+			return container.NewHBox(icon, label)
 		},
 		func(i widget.ListItemID, o fyne.CanvasObject) {
-			o.(*widget.Label).SetText(artistesAffiches[i].Name)
+			box := o.(*fyne.Container)
+			icon := box.Objects[0].(*widget.Icon)
+			label := box.Objects[1].(*widget.Label)
+
+			art := artistesAffiches[i]
+			label.SetText(art.Name)
+
+			if IsLiked(art.ID) {
+				icon.Show()
+			} else {
+				icon.Hide()
+			}
 		},
 	)
 
@@ -135,74 +155,101 @@ func main() {
 			}
 		}
 
-		details := "Artiste : " + art.Name + "\n"
-		details += "Création : " + strconv.Itoa(art.CreationDate) + "\n"
-		details += "Premier album : " + art.FirstAlbum + "\n\n"
+		btnLike := widget.NewButtonWithIcon("Mettre en favoris", theme.ContentAddIcon(), nil)
+		updateLikeBtn := func() {
+			if IsLiked(art.ID) {
+				btnLike.SetText("Retirer des favoris")
+				btnLike.SetIcon(theme.DeleteIcon())
+			} else {
+				btnLike.SetText("Mettre en favoris")
+				btnLike.SetIcon(theme.ConfirmIcon())
+			}
+		}
+		updateLikeBtn()
 
-		details += "Concerts et dates:\n"
-		details += "----------------------\n"
+		btnLike.OnTapped = func() {
+			ToggleLike(art.ID)
+			updateLikeBtn()
+			list.Refresh()
+		}
+
+		btnSpotify := widget.NewButtonWithIcon("Ecouter sur Spotify", theme.MediaPlayIcon(), func() {
+			searchURL := "https://open.spotify.com/search/" + url.QueryEscape(art.Name)
+			u, _ := url.Parse(searchURL)
+			myApp.OpenURL(u)
+		})
+
+		infoContainer := container.NewVBox(
+			widget.NewLabelWithStyle("🎤 "+art.Name, fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+			container.NewGridWithColumns(2, btnLike, btnSpotify),
+			widget.NewSeparator(),
+			widget.NewLabel("Création : "+strconv.Itoa(art.CreationDate)),
+			widget.NewLabel("1er Album : "+art.FirstAlbum),
+			widget.NewLabel("Membres : "+strings.Join(art.Members, ", ")),
+			widget.NewSeparator(),
+			widget.NewLabelWithStyle("Concerts & Géolocalisation", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
+		)
+
+		datesContainer := container.NewVBox()
 
 		for ville, dates := range rel.DatesLocations {
 			villePropre := strings.ToUpper(strings.ReplaceAll(ville, "-", " / "))
 			villePropre = strings.ReplaceAll(villePropre, "_", " ")
 
-			details += "📍 " + villePropre + " :\n"
+			btnMap := widget.NewButtonWithIcon("Voir carte", theme.SearchIcon(), func() {
+				mapURL := "https://www.google.com/maps/search/?api=1&query=" + url.QueryEscape(villePropre)
+				u, _ := url.Parse(mapURL)
+				myApp.OpenURL(u)
+			})
+
+			datesTxt := ""
 			for _, d := range dates {
-				details += "   ▪ " + d + "\n"
+				datesTxt += "   📅 " + d + "\n"
 			}
-			details += "\n"
+			labelDates := widget.NewLabel(datesTxt)
+
+			headerVille := container.NewBorder(nil, nil, widget.NewLabel("+ "+villePropre), btnMap)
+
+			datesContainer.Add(headerVille)
+			datesContainer.Add(labelDates)
+			datesContainer.Add(widget.NewSeparator())
 		}
 
-		labelDetails := widget.NewLabel(details)
-		labelDetails.Wrapping = fyne.TextWrapWord
-
-		scrollContainer := container.NewVScroll(labelDetails)
-		scrollContainer.SetMinSize(fyne.NewSize(300, 400))
+		finalContent := container.NewVBox(infoContainer, datesContainer)
+		scroll := container.NewVScroll(finalContent)
+		scroll.SetMinSize(fyne.NewSize(350, 500))
 
 		var popup *widget.PopUp
-		closeButton := widget.NewButton("Fermer", func() {
+		btnClose := widget.NewButton("Fermer", func() {
 			popup.Hide()
 			list.Unselect(id)
 		})
 
-		content := container.NewBorder(
-			nil, closeButton, nil, nil,
-			scrollContainer,
-		)
-
-		popup = widget.NewModalPopUp(content, myWindow.Canvas())
+		popupContent := container.NewBorder(nil, btnClose, nil, nil, scroll)
+		popup = widget.NewModalPopUp(popupContent, myWindow.Canvas())
 		popup.Show()
 	}
 
 	filtresBox := container.NewVBox(
-		labelDateCreation,
-		sliderDateMin,
-		sliderDateMax,
-		labelDateValues,
+		checkFavorites,
 		widget.NewSeparator(),
-		labelAlbum,
-		sliderAlbumMin,
-		sliderAlbumMax,
-		labelAlbumValues,
+		labelDateCreation, sliderDateMin, sliderDateMax, labelDateValues,
 		widget.NewSeparator(),
-		labelMembres,
-		sliderMembresMin,
-		sliderMembresMax,
-		labelMembresValues,
+		labelAlbum, sliderAlbumMin, sliderAlbumMax, labelAlbumValues,
 		widget.NewSeparator(),
-		labelLieu,
-		selectLieu,
+		labelMembres, sliderMembresMin, sliderMembresMax, labelMembresValues,
+		widget.NewSeparator(),
+		labelLieu, selectLieu,
 		boutonFiltrer,
 	)
 
 	filtresScroll := container.NewVScroll(filtresBox)
-	filtresScroll.SetMinSize(fyne.NewSize(200, 0))
+	filtresScroll.SetMinSize(fyne.NewSize(260, 0))
 
 	contenuPrincipal := container.NewBorder(barreRecherche, nil, nil, nil, list)
 
 	contenuFinal := container.NewBorder(nil, nil, filtresScroll, nil, contenuPrincipal)
 
 	myWindow.SetContent(contenuFinal)
-
 	myWindow.ShowAndRun()
 }
